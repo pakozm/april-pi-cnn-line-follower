c1fc87d95fc1c0e901facdad24625cca0ac9a3a0
Paco Zamora Martinez <pakozm@gmail.com>
2014-04-17 11:53:22 +0200

    Added nets

--- control.lua
+++ control.lua
@@ -2,23 +2,17 @@ local brickpi = require "brickpi"
 --
 local images_dir = "%simages"%{ arg[0]:get_path() }
 --
-local weights_seed        = 1234
+local filename            = arg[1] or "nets/default.net"
 local perturbation_seed   = 5678
-local w_inf               = -0.1
-local w_sup               =  0.1
 local learning_rate       = 0.1
 local momentum            = 0.2
 local weight_decay        = 1e-04
 local L1_norm             = 1e-05
 local max_norm_penalty    = 4
-local ann_type            = "perceptron" -- perceptron, mlp, cnn
-local activation_function = "relu"
---
-local weights_random      = random(weights_seed)
 local perturbation_random = random(perturbation_seed)
 
 -- BrickPi params
-local BLACK            = 0.5
+local BLACK            = 500
 local HISTORY_DISCOUNT = 0.5
 local SLEEP            = 0.1
 local LEFT_MOTOR       = brickpi.PORT_A
@@ -35,77 +29,8 @@ local ALPHA           = 0.1
 local PENALTY = -1
 local REWARD  =  1
 
--- ANN topology
-local PLANES=3
-local HEIGHT=32
-local WIDTH=32
-local INPUT_SIZES = { HEIGHT, WIDTH, PLANES }
-local SIZE = iterator(ipairs(INPUT_SIZES)):select(2):reduce(math.mul(),1)
--- RGB image ( 3 input planes, 5x5 kernel ) => nconv1 output planes
-local conv1  = {5, 5, 3}
-local nconv1 = 10
--- max-pooling 2x2 kernel
-local maxp1  = {1, 2, 2}
--- ( nconv1 input planes, 3x3 kernel ) => nconv2 output planes
-local conv2  = {nconv1, 3, 3}
-local nconv2 = 20
--- max-pooling 2x2 kernel
-local maxp2  = {1, 2, 2}
-local hidden = 100
-
-local thenet = ann.components.stack()
-
-if ann_type == "cnn" then
-  thenet:push( ann.components.convolution{ kernel=conv1, n=nconv1,
-                                           weights="w1",
-                                           input_planes_dim = 3 } ):
-  push( ann.components.convolution_bias{ n=nconv1, ndims=#conv1,
-                                         weights="b1" } ):
-  push( ann.components.actf[activation_function]() ):
-  push( ann.components.max_pooling{ kernel=maxp1 } ):
-  push( ann.components.convolution{ kernel=conv2, n=nconv2,
-                                    weights="w2" } ):
-  push( ann.components.convolution_bias{ n=nconv2, ndims=#conv2,
-                                         weights="b2" } ):
-  push( ann.components.actf[activation_function]() ):
-  push( ann.components.max_pooling{ kernel=maxp2 } ):
-  push( ann.components.flatten() )
-  
-  local convolution_output_size = thenet:precompute_output_size(INPUT_SIZES)[1]
-  
-  thenet:
-  push( ann.components.hyperplane{ input=convolution_output_size,
-                                   output=hidden,
-                                   bias_weights="b3",
-                                   dot_product_weights="w3" } ):
-  push( ann.components.actf[activation_function]() ):
-  push( ann.components.hyperplane{ input=hidden,
-                                   output=NACTIONS,
-                                   bias_weights="b4",
-                                   dot_product_weights="w4" } )
-elseif ann_type == "mlp" then
-  thenet:push( ann.components.flatten() ):
-  push( ann.components.hyperplane{ input=SIZE,
-                                   output=hidden,
-                                   bias_weights="b1",
-                                   dot_product_weights="w1" } ):
-  push( ann.components.actf[activation_function]() ):
-  push( ann.components.hyperplane{ input=hidden,
-                                   output=NACTIONS,
-                                   bias_weights="b2",
-                                   dot_product_weights="w2" } )
-elseif ann_type == "perceptron" then
-  thenet:push( ann.components.flatten() ):
-  push( ann.components.hyperplane{ input=SIZE,
-                                   output=NACTIONS,
-                                   bias_weights="b1",
-                                   dot_product_weights="w1" } )
-else
-  error("Unknown ann_type= " .. ann_type)
-end
-
-local trainer = trainable.supervised_trainer(thenet)
-trainer:build()
+--
+local trainer = util.deserialize(filename)
 trainer:set_option("learning_rate",     learning_rate)
 trainer:set_option("momentum",          momentum)
 trainer:set_option("weight_decay",      weight_decay)
@@ -115,33 +40,7 @@ trainer:set_option("max_norm_penalty",  max_norm_penalty)
 trainer:set_layerwise_option("b.", "weight_decay",     0.0)
 trainer:set_layerwise_option("b.", "max_norm_penalty", 0.0)
 trainer:set_layerwise_option("b.", "L1_norm",          0.0)
---
-if ann_type ~= "perceptron" then
-  trainer:randomize_weights{
-    name_match  = "w.*",
-    random      = weights_random,
-    inf         = w_inf,
-    sup         = w_sup,
-    use_fanin   = true,
-    use_fanout  = true,
-  }
-  trainer:randomize_weights{
-    name_match  = "b.*",
-    random      = weights_random,
-    inf         = 0,
-    sup         = 0.2,
-    use_fanin   = true,
-    use_fanout  = true,
-  }
-else
-  trainer:randomize_weights{
-    random      = weights_random,
-    inf         = w_inf,
-    sup         = w_sup,
-    use_fanin   = true,
-    use_fanout  = true,
-  }
-end
+
 --
 local optimizer = trainer:get_optimizer()
 
@@ -226,6 +125,7 @@ while true do
   local output = thenet:forward(input):get_matrix()
   local reward = compute_reward()
   update(prev_output, prev_action, output, reward)
+  trainer:save(filename, "binary")
   --
   prev_output = output
   prev_action = action
---
+++ generate_net.lua
@@ -0,0 +1,112 @@
+local filename     = arg[1] or "nets/default.net"
+local ann_type     = arg[2] or "perceptron" -- perceptron, mlp, cnn
+local weights_seed = 1234
+--
+local activation_function = "relu"
+local w_inf               = -0.1
+local w_sup               =  0.1
+local weights_random      = random(weights_seed)
+
+-- ANN topology
+local PLANES=3
+local HEIGHT=32
+local WIDTH=32
+local INPUT_SIZES = { HEIGHT, WIDTH, PLANES }
+local SIZE = iterator(ipairs(INPUT_SIZES)):select(2):reduce(math.mul(),1)
+local NACTIONS = 3
+
+-- RGB image ( 3 input planes, 5x5 kernel ) => nconv1 output planes
+local conv1  = {5, 5, 3}
+local nconv1 = 10
+-- max-pooling 2x2 kernel
+local maxp1  = {1, 2, 2}
+-- ( nconv1 input planes, 3x3 kernel ) => nconv2 output planes
+local conv2  = {nconv1, 3, 3}
+local nconv2 = 20
+-- max-pooling 2x2 kernel
+local maxp2  = {1, 2, 2}
+local hidden = 100
+
+local thenet = ann.components.stack()
+
+if ann_type == "cnn" then
+  thenet:push( ann.components.convolution{ kernel=conv1, n=nconv1,
+                                           weights="w1",
+                                           input_planes_dim = 3 } ):
+  push( ann.components.convolution_bias{ n=nconv1, ndims=#conv1,
+                                         weights="b1" } ):
+  push( ann.components.actf[activation_function]() ):
+  push( ann.components.max_pooling{ kernel=maxp1 } ):
+  push( ann.components.convolution{ kernel=conv2, n=nconv2,
+                                    weights="w2" } ):
+  push( ann.components.convolution_bias{ n=nconv2, ndims=#conv2,
+                                         weights="b2" } ):
+  push( ann.components.actf[activation_function]() ):
+  push( ann.components.max_pooling{ kernel=maxp2 } ):
+  push( ann.components.flatten() )
+  
+  local convolution_output_size = thenet:precompute_output_size(INPUT_SIZES)[1]
+  
+  thenet:
+  push( ann.components.hyperplane{ input=convolution_output_size,
+                                   output=hidden,
+                                   bias_weights="b3",
+                                   dot_product_weights="w3" } ):
+  push( ann.components.actf[activation_function]() ):
+  push( ann.components.hyperplane{ input=hidden,
+                                   output=NACTIONS,
+                                   bias_weights="b4",
+                                   dot_product_weights="w4" } )
+elseif ann_type == "mlp" then
+  thenet:push( ann.components.flatten() ):
+  push( ann.components.hyperplane{ input=SIZE,
+                                   output=hidden,
+                                   bias_weights="b1",
+                                   dot_product_weights="w1" } ):
+  push( ann.components.actf[activation_function]() ):
+  push( ann.components.hyperplane{ input=hidden,
+                                   output=NACTIONS,
+                                   bias_weights="b2",
+                                   dot_product_weights="w2" } )
+elseif ann_type == "perceptron" then
+  thenet:push( ann.components.flatten() ):
+  push( ann.components.hyperplane{ input=SIZE,
+                                   output=NACTIONS,
+                                   bias_weights="b1",
+                                   dot_product_weights="w1" } )
+else
+  error("Unknown ann_type= " .. ann_type)
+end
+
+local trainer = trainable.supervised_trainer(thenet)
+trainer:build()
+--
+if ann_type ~= "perceptron" then
+  trainer:randomize_weights{
+    name_match  = "w.*",
+    random      = weights_random,
+    inf         = w_inf,
+    sup         = w_sup,
+    use_fanin   = true,
+    use_fanout  = true,
+  }
+  trainer:randomize_weights{
+    name_match  = "b.*",
+    random      = weights_random,
+    inf         = 0,
+    sup         = 0.2,
+    use_fanin   = true,
+    use_fanout  = true,
+  }
+else
+  trainer:randomize_weights{
+    random      = weights_random,
+    inf         = w_inf,
+    sup         = w_sup,
+    use_fanin   = true,
+    use_fanout  = true,
+  }
+end
+--
+if io.open(filename) then error("Unable to overwrite filename: " .. filename) end
+trainer:save(filename, "binary")
